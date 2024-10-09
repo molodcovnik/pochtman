e6ab8c1e052d24db3a5fe9e61ec5d19b9d3b5adf
Nikolai <120054509+molodcovnik@users.noreply.github.com>
2024-10-09 16:24:56 +0300

    Actions (#2)
    * feat: Добавил workflows
    
    * fix: Добавил workflows
    
    * Bump version
    
    * fix: Переименовал workflows
    
    ---------
    
    Co-authored-by: molodcovnik <molodcovnik@users.noreply.github.com>

--- Mail_Service/core/settings.py
+++ Mail_Service/core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', default='q44)&zyr^z5jf@6cphsvw52lxiy$z8cq%kjc#9(2h&qd-nol-m')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
